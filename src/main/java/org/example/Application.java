c131d38af3602cc9573d64c6b29d9ed8160f838f
quymv <codelabs88@gmail.com>
2022-12-03 13:24:55 +0700

    commit

---
+++ src/main/java/org/example/Application.java
@@ -0,0 +1,48 @@
+package org.example;
+
+
+import org.redisson.Redisson;
+import org.redisson.api.RRateLimiter;
+import org.redisson.api.RateIntervalUnit;
+import org.redisson.api.RateType;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static java.lang.Thread.sleep;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://localhost:6379");
+        RedissonClient redisson = Redisson.create(config);
+
+        RRateLimiter limiter = redisson.getRateLimiter("sms::rate::limiter::us_1123");
+
+        // 1 permits per 15 seconds
+        limiter.trySetRate(RateType.OVERALL, 1, 15, RateIntervalUnit.SECONDS);
+        AtomicInteger succeedCounter = new AtomicInteger(0);
+
+        new Thread(() -> {
+            while (true) {
+
+                if (limiter.tryAcquire()) {
+                    succeedCounter.incrementAndGet();
+                }
+
+                System.out.println(succeedCounter.get());
+
+                try {
+                    sleep(1000L);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+
+    }
+
+}
